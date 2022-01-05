640c4b14891bc5e907e09b24e18f79c05d237015
yuki <96366114+y-anim16@users.noreply.github.com>
2022-01-06 01:04:51 +0900

    update F5キーでシルエット表示できるように(もろもろ問題点あり)

--- Addons/ShowSilhouetteCommand.py
+++ Addons/ShowSilhouetteCommand.py
@@ -1,5 +1,5 @@
 import bpy
-from bpy.props import (IntProperty)
+from bpy.props import (BoolProperty, EnumProperty, IntProperty, PointerProperty)
 
 bl_info = {
     "name": "ShowSilhouette",
@@ -16,6 +16,24 @@ bl_info = {
 }
 
 addon_keymaps = []
+ViewPortLight = 'STUDIO'
+ShadingColorType = 'MATERIAL'
+ShadingSingleColor = (0.799338, 0.799338, 0.799338)
+ViewBackGround = 'MATCAP'
+ViewBackColor = (0.050, 0.050, 0.050) #これはグローバルじゃなくても良さそうな
+ShowBackfaceCulling = False
+ShowXray = False
+ShowShadows = False
+ShowCavity = False
+UseDof = False
+ShowObjectOutline = True
+
+class MOI_Properties(bpy.types.PropertyGroup):
+    pressing = BoolProperty(
+        name="一定間隔でオブジェクト移動中",
+        description="一定間隔でオブジェクト移動中か？",
+        default=False
+    )
 
 class ShowSilhouette(bpy.types.Operator):
     bl_idname = "object.show_silhouette"
@@ -24,11 +42,55 @@ class ShowSilhouette(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
-        bpy.context.space_data.shading.light = 'FLAT'
-        #bpy.ops.mesh.primitive_ico_sphere_add()
-        print("サンプル 2-1: ICO球を生成しました。")
+        sc = context.scene
+        props = sc.moi_props
+        if props.pressing:
+            return {'CANCELLED'}
+        else :
+            props.pressing = True
+            global ViewPortLight
+            global ShadingColorType
+            global ShadingSingleColor
+            global ViewBackGround
+            global ViewBackColor
 
-        return {'FINISHED'}
+            global ShowBackfaceCulling
+            global ShowXray
+            global ShowShadows
+            global ShowCavity
+            global UseDof
+            global ShowObjectOutline
+            
+            ViewPortLight = bpy.context.space_data.shading.light
+            ShadingColorType = bpy.context.space_data.shading.color_type
+            ShadingSingleColor = bpy.context.space_data.shading.single_color
+            ViewBackGround = bpy.context.space_data.shading.background_type
+            ViewBackColor = bpy.context.space_data.shading.background_color
+
+            ShowBackfaceCulling = bpy.context.space_data.shading.show_backface_culling
+            ShowXray = bpy.context.space_data.shading.show_xray
+            ShowShadows = bpy.context.space_data.shading.show_shadows
+            ShowCavity = bpy.context.space_data.shading.show_cavity
+            UseDof = bpy.context.space_data.shading.use_dof
+            ShowObjectOutline = bpy.context.space_data.shading.show_object_outline
+
+            #色が元に戻らない
+            bpy.context.space_data.shading.light = 'FLAT'
+            bpy.context.space_data.shading.color_type = 'SINGLE'
+            bpy.context.space_data.shading.single_color = (0, 0, 0)
+            bpy.context.space_data.shading.background_type = 'VIEWPORT'
+            bpy.context.space_data.shading.background_color = (1, 1, 1)
+
+            bpy.context.space_data.shading.show_backface_culling = False
+            bpy.context.space_data.shading.show_xray = False
+            bpy.context.space_data.shading.show_shadows = False
+            bpy.context.space_data.shading.show_cavity = False
+            bpy.context.space_data.shading.use_dof = False
+            bpy.context.space_data.shading.show_object_outline = False
+            #ポーズモードじゃないとき死ぬ
+            bpy.ops.pose.hide(unselected=True)
+            bpy.ops.pose.hide(unselected=False)
+            return {'FINISHED'}
 
 class ShowSilhouette2(bpy.types.Operator):
     bl_idname = "object.show_silhouette2"
@@ -37,11 +99,38 @@ class ShowSilhouette2(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
     
     def execute(self, context):
-        #bpy.context.space_data.shading.light = 'FLAT'
-        bpy.ops.mesh.primitive_ico_sphere_add()
-        print("サンプル 2-1: ICO球を生成しました。")
-
-        return {'FINISHED'}
+        sc = context.scene
+        props = sc.moi_props
+        if props.pressing is False:
+            return {'CANCELLED'}
+        else:
+            props.pressing = False
+            global ViewPortLight
+            global ShadingColorType
+            global ViewBackGround
+            global ViewBackColor
+            
+            global ShowBackfaceCulling
+            global ShowXray
+            global ShowShadows
+            global ShowCavity
+            global UseDof
+            global ShowObjectOutline
+            
+            bpy.context.space_data.shading.light = ViewPortLight
+            bpy.context.space_data.shading.color_type = ShadingColorType
+            bpy.context.space_data.shading.single_color = ShadingSingleColor
+            bpy.context.space_data.shading.background_type = ViewBackGround
+            bpy.context.space_data.shading.background_color = ViewBackColor
+            
+            bpy.context.space_data.shading.show_backface_culling = ShowBackfaceCulling
+            bpy.context.space_data.shading.show_xray = ShowXray
+            bpy.context.space_data.shading.show_shadows = ShowShadows
+            bpy.context.space_data.shading.show_cavity = ShowCavity
+            bpy.context.space_data.shading.use_dof = UseDof
+            bpy.context.space_data.shading.show_object_outline = ShowObjectOutline
+            bpy.ops.pose.reveal()
+            return {'FINISHED'}
 
 class ShowSilhouetteUi(bpy.types.Panel):
 
@@ -57,10 +146,25 @@ class ShowSilhouetteUi(bpy.types.Panel):
         layout.operator(op_cls.bl_idname, text = "ShowSilhouette")
 
 classes = [
+    MOI_Properties,
     ShowSilhouette,
     ShowSilhouette2,
     ShowSilhouetteUi
 ]
+
+def init_props():
+    sc = bpy.types.Scene
+    sc.moi_props = PointerProperty(
+        name="プロパティ",
+        description="本アドオンで利用するプロパティ一覧",
+        type=MOI_Properties
+    )
+
+
+# プロパティの削除
+def clear_props():
+    sc = bpy.types.Scene
+    del sc.moi_props
 
 def register_shortcut():
     wm = bpy.context.window_manager
@@ -95,10 +199,12 @@ def unregister_shortcut():
 def register():
     for c in classes:
         bpy.utils.register_class(c)
+    init_props()
     register_shortcut()
 
 def unregister():
     unregister_shortcut()
+    clear_props()
     for c in classes:
         bpy.utils.unregister_class(c)
 
