d23a3b74c6dba8377be29a97cb1494ec6a8b04c3
rayt16\yuke <96366114+y-anim16@users.noreply.github.com>
2022-09-16 00:55:29 +0900

    update 一応同一オブジェクト内でも頂点座標の共通化を行えるように

--- Addons/VertexCopyPaste.py
+++ Addons/VertexCopyPaste.py
@@ -26,23 +26,13 @@ class CopyAndPasteVertex(bpy.types.Operator):
     
     def execute(self, context):
 
+        if len(bpy.context.selected_objects) <= 0:
+            return {'FINISHED'}
+
         #一度編集モードを切らないと選択した頂点情報を取得できない
         bpy.ops.object.editmode_toggle()
         bpy.ops.object.editmode_toggle() #編集モードに戻す
-
-
-        # activeObj 最後に選択した奴
-        # selected_object かつ activeObj ではないもの →　最初に選択したやつ
-        # activeObj のところに、最初に選択した奴を持っていく
-
-        print('activeObj' + str(bpy.context.active_object))
-        print('selectedObj' + str(bpy.context.selected_objects))
-
-        # active のindex がわかればよいかも
-        # もしくはactive を取り除いたリスト
-
-        # まず、selectedのなかでactiveのindexがいくつかを出す
-        # active の頂点の座標をworldで出す
+        
 
         activeObj = bpy.context.active_object
         activeObjVertices = []
@@ -52,33 +42,7 @@ class CopyAndPasteVertex(bpy.types.Operator):
             vInfo['worldPos'] = activeObj.matrix_world @ selectedV.co
             activeObjVertices.append(vInfo)
 
-            
-        # selectedVertexes = []
-        # for index, obj in enumerate(bpy.context.selected_objects):
-        #     if index == activeObjIndex:
-        #             continue
-
-        #     for selectedV in [v for v in selectedObj.data.vertices if v.select]:
-        #         vertexInfo = {}
-        #         vertexInfo['vertex'] = selectedV
-        #         selectedVertexes.append(vertexInfo)
-
-        #print(selectedVertexes)
-        #print(hoge[0].data.vertices[0].co)
-        #print(hoge[1].data.vertices[2].co)
-
-        #mat = selectedObjects[1].matrix_world
-        #print(mat @ hoge[1].data.vertices[2].co)
-        #//a = [v for v in hoge if v.select]
-        #print(a)
-
-        # for o in hoge:
-        #     selected = [v for v in o if v.select]
-        #     for s in selected:
-        #         print(s)
-
-        #selected が1種や同じObjの頂点が含まれるなら工夫が必要
-
+        # 複数のメッシュ
         if len(bpy.context.selected_objects) > 1:
             for selectedObj in bpy.context.selected_objects:
                 if selectedObj == activeObj:
@@ -92,48 +56,24 @@ class CopyAndPasteVertex(bpy.types.Operator):
 
                 bpy.ops.object.editmode_toggle()
 
-        # if len(selectedVertexes) > 1:
-        #     # for ループで回す
-        #     selectedVIndex = selectedVertexes[0]['vertex'].index
-        #     # world座標にしていたのを対象のオブジェクトのlocal座標に直す
-        #     #localPos = bpy.context.selected_objects[0].matrix_world.inverted() @ selectedVertexes[1]['worldPos']
-        #     #それぞれのローカルに置き換える
-        #     localPos = bpy.context.active_object.matrix_world.inverted() @ selectedVertexes[1]['worldPos']
-        #     lastSelectedVPos = [localPos[0], localPos[1], localPos[2]]
-        #     bpy.ops.object.editmode_toggle()
+        # １つのメッシュ
+        elif len(bpy.context.selected_objects) == 1:
+            selectedVertices = [v for v in bpy.context.object.data.vertices if v.select]
+            if len(selectedVertices) <= 1:
+                return {'FINISHED'}
+            
+            print(selectedVertices)
 
-        #     #bpy.context.selected_objects[0].data.vertices[selectedVIndex].co = lastSelectedVPos
-        #     #ここで変えるのはactiveObject 以外の選択ObjVertex
-        #     bpy.context.active_object.data.vertices[selectedVIndex].co = lastSelectedVPos
+            # TODO: 選択順は関係ない
+            firstSelectedIndex = selectedVertices[0].index
+            lastSelectedPos = [selectedVertices[1].co[0], selectedVertices[1].co[1], selectedVertices[1].co[2]]
+            bpy.ops.object.editmode_toggle()
 
-        #     bpy.ops.object.editmode_toggle()
+            bpy.context.object.data.vertices[firstSelectedIndex].co = lastSelectedPos
 
+            bpy.ops.object.editmode_toggle()
 
-        return {'FINISHED'}
-
-
-# class BerriesUi(bpy.types.Panel):
-#     bl_idname = "object.berries_ui"
-#     bl_label = "Berries"
-#     bl_space_type = 'VIEW_3D'
-#     bl_region_type = 'UI'
-#     bl_category = "Sugar"
-
-#     def draw(self, context):
-#         layout = self.layout
-        
-#         if bpy.context.mode == 'EDIT_MESH':
-#             op_cls = SetOriginToSelected
-#             layout.operator(op_cls.bl_idname, text = "Set origin to selected")
-
-#         if bpy.context.mode == 'OBJECT':
-#             op_cls = AddShowHideKeyFrame
-#             layout.operator(op_cls.bl_idname, text = "Add Show/Hide key frame")
-
-#         #TODO ここで新しいコマンドのボタン登録
-#         #op_cls = NEW_CLASS_NAME
-#         #layout.operator(op_cls.bl_idname, text = "NEW_BUTTON_NAME")
-        
+        return {'FINISHED'}    
 
 classes = [
     CopyAndPasteVertex,
